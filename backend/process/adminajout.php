ba94690fc13cb64993a6ab8fddac13a03b80acc5
cryptoyacine <77320401+cryptoyacine@users.noreply.github.com>
2021-10-19 08:36:09 +0200

    Backend Admin ajout
    -Pour qu'un admin ajoute un utilisateur

--- backend/process/adminajout.php
+++ backend/process/adminajout.php
@@ -8,42 +8,56 @@ try {
 
 
     $pass_hache = crypt($_POST["password"], 'rl');
-
-
+    
 
 
   $user = new utilisateur([
     "username" => $_POST["username"],
-    "password" => $pass_hache ,                                                 //METTRE LES VALEUR DANS LE SETTER
-    "nom" => $_POST["nom"],
+    "password" => $pass_hache ,
+    "nom" => $_POST["nom"],       //RENTRER VALEUR DANS LES SETTERS
     "prenom" => $_POST["prenom"],
     "mail" => $_POST["mail"],
-    "role"=>$_POST["role"],
+    "date_naissance" => $_POST["date_naissance"],
+    "role"=>$_POST["role"]
     ]);
 
 
 
     $man = new manager();
+                                   //INSTANCIER
 
+    $man->adminajout($user);
+    $man->mail($user);
 
-    $man->adminajout($user);                //UTILISER LA METHOD ADMIN AJOUT POUR AJOUTER UN UTTILISATEUR EN TANT QU'ADMIN
 
 
 } catch (Exception $e) {
-  $_SESSION["erreurcase"] = $e->getMessage();
 
-  if ($_SESSION["erreurcase"] == '') {
-    header("Location: ../../frontend/view/adminajout.php");
-    $_SESSION["erreurcase"] = "reussis";
-  }
-
-
+$_SESSION["connect"] ="7";
 
 }
-header("Location: ../../frontend/view/adminajout.php");
+
+if (isset($_SESSION["connect"]) and $_SESSION["connect"] =="7") {
+
+ header("Location: ../../frontend/view/adminajout.php");
+}
 
 
 
+
+else {
+
+header("Location: ../../index.php");
+}
+
+
+
+
+
+
+
+
+ ?>
 
 
 
