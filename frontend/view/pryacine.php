3d65503e9f216c2800e3a0fc3d18addf98731b4a
billy93t <77320340+billy93t@users.noreply.github.com>
2021-10-19 09:25:39 +0200

    pryacine

---
+++ frontend/view/pryacine.php
@@ -0,0 +1,11 @@
+<td>
+                                  <form method="post" action="vue/supprimer.php">
+                                    <input name="nom" value="'.$value['nom'].'" hidden />
+                                    <input name="prenom" value="'.$value['prenom'].'" hidden />
+
+                                    <input type="submit" value="supprimer" />
+                                  </form>
+                                </td>
+                                <td>
+
+                                </td>
