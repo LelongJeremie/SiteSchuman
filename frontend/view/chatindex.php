d11b1b4a46a5c4570aff09990cdece3e891849b6
cryptoyacine <77320401+cryptoyacine@users.noreply.github.com>
2022-03-02 08:57:50 +0100

    bug fix

--- frontend/view/chatindex.php
+++ frontend/view/chatindex.php
@@ -4,13 +4,7 @@
     <!-- Demarrage session avec un test pour savoir si on est connecté -->
 
   <?php include '../include_frontends/navchat.php';  ?>
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({
-google_ad_client: "ca-pub-5139634720777851",
-enable_page_level_ads: true
-});
-</script>
+
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
