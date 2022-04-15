346ed0732b95a58cd40322114c8d2cea2fa9403b
LelongJeremie <72071667+LelongJeremie@users.noreply.github.com>
2022-04-15 09:34:40 +0200

    Update bdd.php

--- backend/model/bdd.php
+++ backend/model/bdd.php
@@ -6,7 +6,7 @@ private $dbh;
 public function __construct()
 {
   try {
-    $this->dbh = new PDO('mysql:host=localhost;dbname=SiteSchumanp', "root","root"); // connexion à la base de donnée
+    $this->dbh = new PDO('mysql:host=localhost:3307;dbname=SiteSchumanp', "root",""); // connexion à la base de donnée
 
   } catch (PDOException $e) {
     echo "Connection failed : ". $e->getMessage();
